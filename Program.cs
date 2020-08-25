2161d5e86d3d04c06100b07293544f047c4dbc87
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-08-24 21:51:53 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            //Class is a blue print
+            //Instantiate new Book();
+            // book is a variable of type new Book
+            var book = new Book("Raul's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();           
+                                    
+            
+        }
+    }
+}
